35d77b66a5e3e4f1c3f4defac734f422cd7e017e
Junhao Guo <guojunhao427@gmail.com>
2023-11-09 12:06:28 -0800

    create and connect with database

---
+++ src/main/java/com/cs362/tuffyride/TuffyRideApplication.java
@@ -0,0 +1,13 @@
+package com.cs362.tuffyride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuffyRideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuffyRideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cs362/tuffyride/TuffyRideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs362.tuffyride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuffyRideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
